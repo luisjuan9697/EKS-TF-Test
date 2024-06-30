8470372625bc989c908336be674e3aca821ba614
Luis <luis.luisjuan@udgvirtual.udg.mx>
2024-06-29 21:38:11 -0600

    add files

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket         = "testcicdk8s"
+    region         = "us-east-1"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
